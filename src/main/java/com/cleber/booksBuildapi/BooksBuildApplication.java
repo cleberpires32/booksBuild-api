290de232d1776a25d1c7c3fffb080db15bb0813b
cleberpires32 <cleberpires269@gmail.com>
2022-05-06 00:08:28 -0300

    commit inicial - criação do projeto

---
+++ src/main/java/com/cleber/booksBuildapi/BooksBuildApplication.java
@@ -0,0 +1,13 @@
+package com.cleber.booksBuildapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksBuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleber/booksBuildapi/BooksBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleber.booksBuildapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
