a0f722bbecee42181aef1e1bcbb8e8c1870b383f
cleberpires32 <cleberpires269@gmail.com>
2022-05-12 15:49:10 -0300

    ajuste crossorigin

--- src/main/java/com/cleber/booksBuildapi/domain/Categoria.java
+++ src/main/java/com/cleber/booksBuildapi/domain/Categoria.java
@@ -14,9 +14,11 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@CrossOrigin("*")
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable{
