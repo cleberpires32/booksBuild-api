d0f74221e8bbd99d13a3d14169ed12d7cb9a781e
cleberpires32 <cleberpires269@gmail.com>
2022-05-15 22:17:13 -0300

    adicionando @CrossOrin(*) em CategoriaResource

--- src/main/java/com/cleber/booksBuildapi/resource/CategoriaResource.java
+++ src/main/java/com/cleber/booksBuildapi/resource/CategoriaResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import com.cleber.booksBuildapi.dto.CategoriaDTO;
 import com.cleber.booksBuildapi.service.CategoriaService;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
